ef461572d222a0d3e20a81bb1d53b235a96a7059
USE1ESS <help.flosommerfeld@gmail.com>
2017-07-28 14:46:55 +0200

    Fixed a security problem.

--- history_ui/js/index.js
+++ history_ui/js/index.js
@@ -1,35 +1,44 @@
-function onError(error) {//log errors
+function onError(error) { //log errors
   console.log(`Error: ${error}`);
 }
 
-function escapeHTML(str) {//"This method is a last resort which should be used only as a temporary measure in established code bases. It is safe, though inefficient [..]."
-                          //See: https://developer.mozilla.org/en-US/Add-ons/Overlay_Extensions/XUL_School/DOM_Building_and_HTML_Insertion
-     return str.replace(/[&"'<>]/g, (m) => escapeHTML.replacements[m]);
-   }
-   escapeHTML.replacements = {
-     "&": "&amp;",
-     '"': "&quot;",
-     "'": "&#39;",
-     "<": "&lt;",
-     ">": "&gt;"
-   };
-
 function onGot(item) {
   var pages = item.pages;
-  if(pages.length == 0){//remove the loader element from the HTML if there are no saved pages
+  if (pages.length == 0) { //remove the loader element from the HTML if there are no saved pages
     $("#overlay").detach();
-     $("#overlay-alert").fadeIn(1500);//fade in the alert "You currently have no history."
-  }
-  else{
-    for (i in pages) {//iterate every page and add it to the HTML
-      $("#items").prepend("<div title='" + escapeHTML(pages[i].hostname) + "' class='page'><a href='" + escapeHTML(pages[i].url) + "'><img src='" + escapeHTML(pages[i].image) + "'><div class='desc' style='line-height:30px;'>" + escapeHTML(pages[i].title) + "</div></a></div>");
+    $("#overlay-alert").fadeIn(1500); //fade in the alert "You currently have no history."
+  } else {
+    for (i in pages) { //iterate every page and add it to the HTML
+      if (pages[i].url.startsWith('http') && pages[i].image.startsWith('data:image/jpeg;base64,')) { // making sure the url is a url and the image is a data uri graphic
+        let div = document.createElement('div');
+        let div2 = div.cloneNode(false); // making a clone
+
+        div.title = pages[i].hostname;
+        div.className = 'page';
+
+        let a = document.createElement('a');
+        a.href = pages[i].url;
+
+        let img = document.createElement('img');
+        img.src = pages[i].image;
+
+        div2.className = 'desc';
+        div2.setAttribute('style', 'line-height:30px;');
+        div2.textContent = pages[i].title;
+
+        a.appendChild(img);
+        a.appendChild(div2);
+        div.appendChild(a);
+
+        $("#items").prepend(div);//adding the div to the HTML
+      }
     }
-    $("#overlay").detach();//remove the loader element once the iteration is finished
+    $("#overlay").detach(); //remove the loader element once the iteration is finished
   }
 }
 
 
 $(document).ready(function() {
-  let pages = browser.storage.local.get("pages");//get the JSON object
-  pages.then(onGot, onError);//promise
+  let pages = browser.storage.local.get("pages"); //get the JSON object
+  pages.then(onGot, onError); //promise
 });
